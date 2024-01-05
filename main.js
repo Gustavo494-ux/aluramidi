51d1c6b29180bab7879be2c8145ff8a15f879305
Gustavo Gama <gustavogama494@gmail.com>
2024-01-05 17:04:22 -0300

    modulo 3

--- main.js
+++ main.js
@@ -2,8 +2,14 @@ function tocaSomPom() {
     document.querySelector('#som_tecla_pom').play();       
 }
 
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
+const listaDeTeclas = document.querySelectorAll('.tecla');
+listaDeTeclas[0].onclick = tocaSomPom;
+// ListaDeTeclas.forEach(element => {
+//     element.onclick = tocaSomPom
+// });
 
+for (let index = 0; index < ListaDeTeclas.length; index++) {
+    ListaDeTeclas[index].onclick = tocaSomPom;
 
-
+}
 
