f77f7a43d8d26cbc23bc0a0bf5de9c9ea46b4dad
Gustavo Gama <gustavogama494@gmail.com>
2024-01-06 14:30:56 -0300

    modulo 5

--- main.js
+++ main.js
@@ -1,11 +1,28 @@
 function tocaSom(idElementoAudio) {
-    document.querySelector(idElementoAudio).play();       
+    const elemento = document.querySelector(idElementoAudio);
+
+    if (elemento.localName === 'audio' && elemento) {
+        elemento.play()
+    } else {
+        console.log('elemento não encontrado')
+    }
+
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
+listaDeTeclas.forEach(tecla => {
+    tecla.onclick = function () {
+        tocaSom(`#som_${tecla.classList[1]}`)
+    }
 
-listaDeTeclas.forEach(element => {
-    element.onclick = function(){
-        tocaSom(`#som_${element.classList[1]}`)
-    }        
+    tecla.onkeydown = function (evento) {
+        console.log(evento)
+        if (evento.Code in ['Enter', 'Space']) {
+            tecla.classList.add('ativa')
+        }
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa')
+    }
 });
